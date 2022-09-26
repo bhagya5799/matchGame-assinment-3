ad7400b1faed9ef702cad2695526146681d57f87
bhagya <lbhagya818@gmail.com>
2022-09-26 08:21:09 +0000

    newcode

---
+++ src/Components/Components/MatchGame/index.js
@@ -0,0 +1,35 @@
+import './index.css'
+
+// const ImageList = props => {
+//   const {listOfImageDetails, onChangeScore} = props
+//   const {thumbnailUrl, id} = listOfImageDetails
+
+//   const onClickIdScore = () => {
+//     onChangeScore(id)
+//   }
+
+//   return (
+//     <li>
+//       <button className="btn-image" type="button" onClick={onClickIdScore}>
+//         <img src={thumbnailUrl} alt="thumbnail" className="list-img" />
+//       </button>
+//     </li>
+//   )
+// }
+
+const ImageList = props => {
+  const {listOfImageDetails, onChangeScore} = props
+  const {thumbnailUrl, id} = listOfImageDetails
+  const onClickthumbnailBtn = () => {
+    onChangeScore(id)
+  }
+  return (
+    <li>
+      <button className="buton-url" onClick={onClickthumbnailBtn} type="button">
+        <img src={thumbnailUrl} className="list-img" alt="thumbnail" />
+      </button>
+    </li>
+  )
+}
+
+export default ImageList
